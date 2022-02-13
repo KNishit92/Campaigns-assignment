519c329ac0f62105b3f09c1a5bbf649bfbd7ba19
Kumar Nishit <kumarnishit870@gmail.com>
2022-02-13 15:19:21 +0530

    update routes

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ var app = express();
 app.set("port", process.env.PORT || 3000);
 
 app.use(express.json())
-app.use('/api/v1/campaign', routes);
-app.use('/api/v1/campaign/active', routes);
-app.use('/api/v1/campaign/closed', routes);
+app.use('/api/v1/campaigns', routes);
+app.use('/api/v1/campaigns/active', routes);
+app.use('/api/v1/campaigns/closed', routes);
 
 app.listen(app.get("port"), function(){
     console.log(`Server started listening on port: ${app.get("port")}`);
